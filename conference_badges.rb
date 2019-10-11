36e5ab1a0103859cf1db381f3d5e2aac1c4abd97
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-11 18:32:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,8 @@ end
 def assing_rooms(rooms)
   speakers = ["Edsger","Ada","Charles","Grace","Linus","Matz"]
   rooms = [1,2,3,4,5,6,7]
-  speakers.each_with_index {|speakers, rooms|
+  speakers.each_with_index {|speakers, rooms|}
+  
 
 #badge_maker("Ariel")
 #puts "Hello, my name is #{name}."
