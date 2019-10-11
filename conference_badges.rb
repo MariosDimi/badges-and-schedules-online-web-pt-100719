ebbdb14f512d3e07ed10fae91921a5cb7eed7d9f
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-11 18:30:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,8 @@ def batch_badge_creator(speakers)
 end
 
 def assing_rooms(rooms)
-  speakers.each_with_index {
+  
+  speakers.each_with_index 
 
 #badge_maker("Ariel")
 #puts "Hello, my name is #{name}."
