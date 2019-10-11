5f1146cce41fe2b65d54a88e93ee661dabd2351d
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-11 18:27:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,2 +1,13 @@
-badge_maker("Ariel")
-puts "Hello, my name is #{name}."
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  speakers = ["Edsger","Ada","Charles","Grace","Linus","Matz"]
+  speakers.each do |speaker|
+    puts "Hello, my name is #{speaker}!"
+end
+
+
+#badge_maker("Ariel")
+#puts "Hello, my name is #{name}."
